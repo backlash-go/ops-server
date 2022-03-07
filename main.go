d3d14af15c2a6218dbf74dd78c1a0877898b7751
backlash <xixianbing@hexiaoxiang.com>
2022-03-07 16:32:06 +0800

    sad

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!!!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
